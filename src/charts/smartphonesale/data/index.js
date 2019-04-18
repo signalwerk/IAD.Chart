610283d00bd788a119059c14bead9ba1a3e70f2e
Stefan Huber <sh@signalwerk.ch>
2019-04-18 18:08:16 +0200

    MOD: adjust smartphone sales

--- src/charts/smartphonesale/data/index.js
+++ src/charts/smartphonesale/data/index.js
@@ -1,90 +1,87 @@
-import _ from 'lodash';
-import smartphoneQuarters from './smartphone_quarters.json'
-import smartphoneYears from './smartphone_years.json'
-import desktopYears from './desktop_years.json'
-import vendorYears from './vendor_years.json'
+import _ from "lodash";
+import smartphoneQuarters from "./smartphone_quarters.json";
+import smartphoneYears from "./smartphone_years.json";
+import desktopYears from "./desktop_years.json";
+import vendorYears from "./vendor_years.json";
 
 const maxYear = 2016;
 
 class Data {
-
   vendorDesktopShipment() {
-
-  return _(vendorYears.shipments)
-    .map(item => ({
-      year: item.year,
-      total: (item.Lenovo || 0)
-              + (item['HP Inc'] || 0)
-              + (item.Dell || 0)
-              + (item.Asus || 0)
-              + (item.Apple || 0)
-              + (item.Acer || 0)
-              + (item.Toshiba || 0)
-              + (item.Others || 0)
-    }))
-    .filter( item => item.year >= 2007) // 2009 is not complete
-    .value();
+    return _(vendorYears.shipments)
+      .map(item => ({
+        year: item.year,
+        total:
+          ((item.Lenovo || 0) +
+          (item["HP Inc"] || 0) +
+          (item.Dell || 0) +
+          (item.Asus || 0) +
+          (item.Apple || 0) +
+          (item.Acer || 0) +
+          (item.Toshiba || 0) +
+          (item.Others || 0)) * 1000000
+      }))
+      .filter(item => item.year >= 2007) // 2009 is not complete
+      .value();
   }
 
-
-
   desktopShipment() {
-
-    var result = this.vendorDesktopShipment()
+    var result = this.vendorDesktopShipment();
 
     // find first year we have detail data
-    let lastYear = _.maxBy(result, item => item.year).year
+    let lastYear = _.maxBy(result, item => item.year).year;
 
     // push rest data
     _(desktopYears.shipments)
-      .filter( item => item.Year > lastYear)
+      .filter(item => item.Year > lastYear)
       .forEach(item => {
         result.push({
           year: item.Year,
-          total: item.Laptops + item["Desktop-PCs"],
-        })
+          total: (item.Laptops + item["Desktop-PCs"]) * 1000000
+        });
       });
 
-      return result;
+    return result;
   }
 
   smartphoneYears() {
     return _(this.smartphoneSalesByYear())
-      .map('year')
+      .map("year")
       .uniq()
       .value();
   }
 
   smartphoneSalesByYear() {
-
     // sum by year
     var result = _(smartphoneQuarters.sales)
-      .map('Year')
+      .map("Year")
       .uniq()
-      .filter( item => item > 2009) // 2009 is not complete
-      .filter( item => item <= maxYear)
+      .filter(item => item > 2009) // 2009 is not complete
+      .filter(item => item <= maxYear)
       .map(key => ({
         year: key,
-        total: _(smartphoneQuarters.sales).filter({ Year: key }).sumBy('total')
+        total: _(smartphoneQuarters.sales)
+          .filter({ Year: key })
+          .sumBy("total") * 1000000
       }))
       .value();
 
-      // find first year we have detail data
-      let firstYear = _.minBy(result, item => item.year).year
+    // find first year we have detail data
+    let firstYear = _.minBy(result, item => item.year).year;
 
-      // push old data
-      _(smartphoneYears.sales)
-        .filter( item => item.year < firstYear)
+    // push old data
+    _(smartphoneYears.sales)
+      .filter(item => item.year < firstYear)
       .forEach(item => {
         result.push({
           year: item.year,
-          total: item.unitsInMio
-        })
-      })
+          total: item.unitsInMio * 1000000
+        });
+      });
 
-      return _.sortBy(result, ['year']);
+    return _.sortBy(result, ["year"]);
   }
 }
 
-var data = new Data;
+var data = new Data();
 export default data;
--- src/charts/smartphonesale/index.js
+++ src/charts/smartphonesale/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Data from "./data";
-import ThemeSignalwerk, {  strokes } from "../theme";
+import ThemeSignalwerk, {
+  colors,
+  strokes,
+  defaultFont,
+  labelStyles,
+  blockStyles
+} from "../theme";
 import { yAxis } from "../theme";
+import { abbreviate } from "../../utility/abbreviateNumber";
 import {
   VictoryBar,
   VictoryContainer,
@@ -15,16 +22,49 @@ import {
 
 export default class Smartphonesale extends React.Component {
   render() {
-    console.log("desktopShipment", Data.desktopShipment());
+    // let countTicks = Array.from(
+    //   { length: 17 },
+    //   (item, index) => index * 100000000
+    // ).filter((item, index) => index % 2 === 0);
+
+    let countTicks = [0, 1, 2, 3, 4, 6, 8, 10, 12, 14, 16].map(
+      (item, index) => item * 100000000
+    );
+
+    // console.log("desktopShipment", Data.desktopShipment());
+    console.log("countTicks", countTicks);
 
     return (
       <div>
-        <VictoryChart domainPadding={{ x: [5, 30] }} theme={ThemeSignalwerk}>
+        <VictoryChart
+          height={400 + 75}
+          padding={{ top: 50, left: 60, right: 50, bottom: 50 + 75 }}
+          domainPadding={{ x: [5, 30] }}
+          theme={ThemeSignalwerk}
+        >
+
+          <VictoryLabel
+            x={55}
+            y={420}
+            style={blockStyles}
+            lineHeight={(1.4 / 24) * 12}
+            text={["■", "■"]}
+          />
+          <VictoryLabel
+            x={80}
+            y={420}
+            lineHeight={1.4}
+            style={{ fill: "black", fontSize: "12px", fontFamily: defaultFont }}
+            text={["Desktop Computer", "Smartphones"]}
+          />
+
+
           <VictoryAxis tickValues={Data.smartphoneYears()} />
 
           <VictoryAxis
             dependentAxis
-            tickValues={[0, 100, 200, 400, 600, 800, 1000, 1200, 1400, 1600]}
+            tickValues={countTicks}
+            tickFormat={t => `${abbreviate(t)}`}
           />
 
           <VictoryLine
