768a31442f123ebc14ea9fa0b2754d7115e9a22c
Stefan Huber <sh@signalwerk.ch>
2019-09-06 16:33:35 +0200

    Create incomeHF2019_Semester1.js

---
+++ src/charts/income/incomeHF2019_Semester1.js
@@ -0,0 +1,22 @@
+// http://ramdajs.com/docs/
+
+export const IAD2019_S1 = {
+  caption: "1. Semester",
+  comment: "eine person hat ungültig ausgefüllt 60.5k? oder 90.5k?",
+  data: [
+    { CHF: 22000, gender: "f" },
+    { CHF: 54000, gender: "f" },
+    { CHF: 67600, gender: "f" },
+    { CHF: 73200, gender: "m" },
+    { CHF: 62400, gender: "f" },
+    { CHF: 82875, gender: "f" },
+    { CHF: 96450, gender: "m" },
+    { CHF: 59800, gender: "m" },
+    { CHF: 61100, gender: "f" },
+    { CHF: 33600, gender: "m" },
+    { CHF: 61100, gender: "f" },
+    { CHF: 58500, gender: "m" },
+    { CHF: 62400, gender: "f" },
+    { CHF: 70200, gender: "f" }
+  ]
+};
